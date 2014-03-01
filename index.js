cf33fdb33f9f2a999dcfa8f4bab51ce03dafa2c8
bredele <olivier.wietrich@gmail.com>
2014-02-28 22:37:36 -0700

    add event brick

---
+++ index.js
@@ -0,0 +1,42 @@
+var events = require('events-brick');
+var names = [
+	'click',
+	'dblclick',
+	'mousedown',
+	'mouseup',
+	'keydown',
+	'keypress',
+	'keyup',
+  'change',
+  'blur',
+  'focus',
+  'input',
+  'submit'
+  ];
+
+/**
+ * Expose 'Bricks'
+ */
+
+module.exports = function(scope) {
+	return function(view) {
+		addEvents(scope, view);
+	};
+};
+
+
+function addEvents(scope, view) {
+	var ev = events(scope || view);
+	view.add('on', ev);
+	for(var l = names.length; l--;) {
+		var name = names[l];
+		view.add('on-' + name, function(node, fn) {
+			//should we do use capture?
+			ev.on(node, name, fn);
+		});
+	}
+}
+
+// function Bricks() {
+// 	this.ev = 
+// }
